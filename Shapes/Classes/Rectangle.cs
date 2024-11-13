e25a644fd1a29635aa76b36e0861887edc71e738
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2024-11-13 15:26:30 +0000

    Add project files.

---
+++ Shapes/Classes/Rectangle.cs
@@ -0,0 +1,58 @@
+﻿using System.Drawing;
+
+namespace Shapes.Classes
+{
+    internal class Rectangle : Shape
+    {
+        private int _width;
+        private int _height;
+        private int _area;
+        private int _circumference;
+
+        public int Width
+        { get { return _width; }
+            private set { _width = value; }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+            private set { _height = value; }
+        }
+
+        public int Area
+        {
+            get { return _area; }
+            private set { _area = value; }
+        }
+
+        public int Circumference
+        {
+            get { return _circumference; }
+            private set { _circumference = value; }
+        }
+
+        public Rectangle(Point position, Color colour, int width, int height) : base(position, colour)
+        {
+            Width = width;
+            Height = height;
+            this.Area = CalculateArea();
+            this.Circumference = CalculateCircumference();
+
+        }
+
+        private int CalculateArea()
+        {
+            return _width * _height;
+        }
+
+        private int CalculateCircumference()
+        {
+            return = 2 * (_width + _height);
+        }
+    }
+
+}
+
+}
+}
---
+++ Shapes/Classes/Shape.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+
+namespace Shapes.Classes
+{
+    internal class Shape
+    {
+        private Color _colour;
+        private Point _position;
+
+        public Color Colour 
+        { 
+            get { return _colour; }
+            private set { _colour = value; }
+        }
+
+        public Point Position
+        {
+            get { return _position; }
+            private set { _position = value; }
+        }
+
+        public Shape(Point position, Color colour)
+        {
+            this.Position = position;
+            this.Colour = colour;
+        }
+
+        public string getCharecteristics() 
+        {
+            return $"The Position of this shape is X{this._position.X} and Y{this._position.Y}.The colour of the shape is {_colour.Name}";   
+        }
+
+    }
+}
